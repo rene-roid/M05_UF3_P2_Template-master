a24e61c7535493d32163b9eb99d9f232660eaf51
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-04-30 11:48:14 +0200

    ccv

--- App_Code/Model/Game.cs
+++ App_Code/Model/Game.cs
@@ -75,5 +75,15 @@ namespace M05_UF3_P2_Template.App_Code.Model
             };
             return DatabaseManager.Insert("Game", fields) > 0 ? true : false;
         }
+
+        public bool Remove()
+        {
+            return Remove(Id);
+        }
+
+        public static bool Remove(int Id)
+        {
+            return DatabaseManager.Delete("Game", Id) > 0 ? true : false;
+        }
     }
 }
